509efd88b46e3ab3f76047d4961b97379d1ce67c
PedroMarchiN <pedromarchinunes@estudante.ufscar.br>
2025-11-07 16:50:05 -0300

    fix: declaration of non existent function isNormalItem

--- src/GildedRose.java
+++ src/GildedRose.java
@@ -7,7 +7,10 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (isNormalItem(items[i])) {
+            if (!items[i].name.equals("Aged Brie")
+                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")
+                    && !items[i].name.equals("Conjured Mana Cake")
+                    && !items[i].name.equals("Eternal Artifact")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
